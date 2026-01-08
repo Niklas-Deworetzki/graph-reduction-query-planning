125e1f086c7f282d6df93d171d63e5599b32ecab
Niklas Deworetzki <nikdew@chalmers.se>
2026-01-08 15:10:10 +0100

    Added evaluation strategy for fully evaluating the graph.

--- src/grqe/evaluation.py
+++ src/grqe/evaluation.py
@@ -1,26 +1,81 @@
-from typing import Callable, Protocol
+from typing import ClassVar, Protocol
 
+from grqe.debug import LOGGER, current_time
 from grqe.fetch import LookupStrategy
 from grqe.korp import Corpus
 from grqe.query import *
-from grqe.debug import LOGGER
-
-
-def index_of_match[T](elements: List[T], predicate: Callable[[T], bool]) -> int:
-    return next(i for i, e in elements if predicate(e))
-
-
-def fold[X](xs: List[X], operator: Callable[[X, X], X]) -> X:
-    res = xs[0]
-    for x in xs[1:]:
-        res = operator(res, x)
-    return res
 
 
 class Evaluator(Protocol):
 
     def eval_fully(self, node: Node) -> Value:
         ...
+
+
+class FullEvaluator:
+    set_operations: ClassVar[dict] = {
+        Conjunction: lambda a, b: a & b,
+        Disjunction: lambda a, b: a | b,
+        Sequence: lambda a, b: a.join(b),
+        Alternative: lambda a, b: a,
+    }
+
+    corpus: Corpus
+
+    def __init__(self, corpus: Corpus):
+        self.corpus = corpus
+        self.lookup_strategy = LookupStrategy.instance(corpus)
+
+    def eval_node(self, node: Node):
+        if node.is_evaluated():
+            return
+
+        match node:
+            case Conjunction() | Disjunction() | Sequence() | Alternative():
+                for child in node.elements:
+                    self.eval_node(child)
+
+                operation = self.set_operations[type(node)]
+
+                start_time = current_time()
+
+                res = node.elements[0].value.copy()
+                for element in node.elements[1:]:
+                    res = operation(res, element.value.copy())
+
+                node.value = res
+
+            case Subtraction():
+                self.eval_node(node.lhs)
+                self.eval_node(node.rhs)
+
+                start_time = current_time()
+
+                node.value = node.lhs.value.copy() - node.rhs.value.copy()
+
+            case Extend():
+                self.eval_node(node.element)
+
+                start_time = current_time()
+                node.value = node.element.value.copy().extend(node.lhs, node.rhs)
+
+            case Lookup():
+                start_time = current_time()
+
+                min_off = min(atom.relative_position for atom in node.atoms)
+                max_off = max(atom.relative_position for atom in node.atoms)
+                lookup_positions = self.lookup_strategy.perform_lookup(node, offset=min_off)
+                node.value = BucketRangeSet({max_off - min_off: lookup_positions})
+
+            case _:
+                raise NotImplementedError()
+
+        elapsed = current_time() - start_time
+        node.time = elapsed
+
+    def eval_fully(self, node: Node) -> Value:
+        self.eval_node(node)
+        return node.value
 
 
 class CostGuidedEvaluator:
@@ -34,7 +89,7 @@ class CostGuidedEvaluator:
 
     def __init__(self, corpus: Corpus):
         self.corpus = corpus
-        self.index_manager = LookupStrategy.instance(corpus)
+        self.lookup_strategy = LookupStrategy.instance(corpus)
 
     def eval_next_in_list(self, elements: List[Node]) -> Node:
         target = min(elements, key=lambda e: e.cost if not e.is_evaluated() else math.inf)
@@ -78,7 +133,7 @@ class CostGuidedEvaluator:
                 LOGGER.debug(f'Starting lookup for {len(node.atoms)} features.')
                 min_off = min(atom.relative_position for atom in node.atoms)
                 max_off = max(atom.relative_position for atom in node.atoms)
-                lookup_positions = self.index_manager.perform_lookup(node, offset=min_off)
+                lookup_positions = self.lookup_strategy.perform_lookup(node, offset=min_off)
                 node.value = BucketRangeSet({max_off - min_off: lookup_positions})
                 LOGGER.debug(f'Lookup finished.')
 
