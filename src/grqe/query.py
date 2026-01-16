4c69b2a46dc87374baf968885d03e2632b6eed4c
Niklas Deworetzki <nikdew@chalmers.se>
2026-01-16 18:21:59 +0100

    Added more metadata to node types.

--- src/grqe/query.py
+++ src/grqe/query.py
@@ -27,7 +27,7 @@ class Node(ABC):
     # Instance fields in this class will be provided for every node.
     # We mark them specially, so that they can be mutable and nodes are still hashable and
     #  compare equal on their fields.
-    __SUPERCLASS_FIELD_MARKER__: ClassVar = field(
+    __inherited_to_subclasses: ClassVar = field(
         init=False,  # Exclude from generated constructor.
         compare=False,  # Exclude from generated comparison.
         hash=False,  # Exclude from generated hash function.
@@ -35,13 +35,16 @@ class Node(ABC):
     )
 
     # Mark all the instance fields for proper behavior.
-    cost: Cost = __SUPERCLASS_FIELD_MARKER__
-    value: Optional[Value] = __SUPERCLASS_FIELD_MARKER__
-    _refcount: int = __SUPERCLASS_FIELD_MARKER__
+    cost: Cost = __inherited_to_subclasses
+    value: Optional[Value] = __inherited_to_subclasses
+    _refcount: int = __inherited_to_subclasses
 
     # Set statically for each subclass to define specific subclass behavior.
     arity: ClassVar[Optional[int]] = None
-    __children_iter__: ClassVar[Callable[['Node'], Iterable['Node']]]
+    is_associative: ClassVar[bool] = False
+    is_commutative: ClassVar[bool] = False
+    is_idempotent: ClassVar[bool] = False
+    _children_iter: ClassVar[Callable[['Node'], Iterable['Node']]]
 
     def __post_init__(self):
         self.cost = math.inf
@@ -50,10 +53,10 @@ class Node(ABC):
         for child in self.children():
             child._refcount += 1
 
-    __known_subtypes__: ClassVar[list[type[Node]]] = []
+    OPERATOR_TYPES: ClassVar[list[type[Node]]] = []
 
     def __init_subclass__(cls):
-        Node.__known_subtypes__.append(cls)
+        Node.OPERATOR_TYPES.append(cls)
 
     def is_evaluated(self) -> bool:
         return self.value is not None
@@ -65,7 +68,7 @@ class Node(ABC):
         return self.value
 
     def children(self) -> Generator['Node']:
-        yield from self.__children_iter__()
+        yield from self._children_iter()
 
     def flatten(self) -> Generator['Node']:
         yield self
@@ -74,13 +77,26 @@ class Node(ABC):
 
     @classmethod
     def construct(cls, elements: Iterable[Node]) -> 'Node':
+        """Create an instance of this class from an iterable of child nodes."""
         if cls.arity is not None:
             return cls(*elements)
         return cls(tuple(elements))
 
     @classmethod
     def class_tag(cls) -> int:
-        return Node.__known_subtypes__.index(cls)
+        return Node.OPERATOR_TYPES.index(cls)
+
+    def __lt__(self, other):
+        return self.signature < other.signature
+
+    def __gt__(self, other):
+        return self.signature > other.signature
+
+    def __le__(self, other):
+        return self.signature <= other.signature
+
+    def __ge__(self, other):
+        return self.signature >= other.signature
 
     @cached_property
     def signature(self) -> tuple:
@@ -89,25 +105,36 @@ class Node(ABC):
         return tag, len(child_tags), *child_tags
 
 
-def node(*fields: str, var_arity: bool = False):
-    """
-    Decorator used to define AST nodes.
-    """
+def node_type(*fields: str,
+              var_arity: bool = False,
+              associative: bool = False,
+              commutative: bool = False,
+              idempotent: bool = False,
+              ):
+    """Decorator used to define AST nodes."""
 
-    def decorate(cls):
+    def decorate(cls: type[Node]):
         # Apply @dataclass(unsafe_hash=True) to the provided class.
         # This generates a fitting hash function, turning the instances hashable.
         cls = dataclass(unsafe_hash=True)(cls)
+        assert issubclass(cls, Node), '@node_type used to define AST node is not subclass of Node.'
 
-        # Set the class-level arity.
+        # Set the class-level fields.
         cls.arity = None if var_arity else len(fields)
+        cls.is_associative = associative
+        cls.is_commutative = commutative
+        cls.is_idempotent = idempotent
+
+        assert var_arity == (associative or commutative or idempotent), \
+            'Only variable arity operators can be associative, commutative or idempotent.'
 
         if var_arity:
             assert len(fields) == 1, 'Variable arity operators are required to have 1 instance variable.'
-            cls.__children_iter__ = lambda self: getattr(self, fields[0])
+            name = fields[0]
+            cls._children_iter = lambda self: getattr(self, name)
         else:
             names = tuple(fields)
-            cls.__children_iter__ = lambda self: (getattr(self, n) for n in names)
+            cls._children_iter = lambda self: (getattr(self, n) for n in names)
 
         __all__.append(cls.__name__)
         return cls
@@ -115,7 +142,7 @@ def node(*fields: str, var_arity: bool = False):
     return decorate
 
 
-@node()
+@node_type()
 class Lookup(Node):
     atoms: Iterable[Atom]
 
@@ -127,43 +154,43 @@ class Lookup(Node):
         return tag, len(immutable_atoms), *immutable_atoms
 
 
-@node('element')
+@node_type('element')
 class Negation(Node):
     element: Node
 
 
-@node('elements', var_arity=True)
+@node_type('elements', var_arity=True, associative=True, commutative=True, idempotent=True)
 class Conjunction(Node):
     elements: Iterable[Node]
 
 
-@node('elements', var_arity=True)
+@node_type('elements', var_arity=True, associative=True, commutative=True, idempotent=True)
 class Disjunction(Node):
     elements: Iterable[Node]
 
 
-@node('elements', var_arity=True)
+@node_type('elements', var_arity=True, associative=True, commutative=True, idempotent=True)
 class Alternative(Node):
     elements: Iterable[Node]
 
 
-@node('elements', var_arity=True)
+@node_type('elements', var_arity=True, associative=True)
 class Sequence(Node):
     elements: Seq[Node]
 
 
-@node('lhs', 'rhs')
+@node_type('lhs', 'rhs')
 class Subtraction(Node):
     lhs: Node
     rhs: Node
 
 
-@node()
+@node_type()
 class Arbitrary(Node):
     pass
 
 
-@node()
+@node_type()
 class Epsilon(Node):
     pass
 
