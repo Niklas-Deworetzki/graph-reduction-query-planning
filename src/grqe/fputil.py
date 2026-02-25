1b12c954da5c3ef51dc7fce005e59190fd62fa46
Niklas Deworetzki <nikdew@chalmers.se>
2026-02-25 17:34:20 +0100

    Removed obsolete fold implementation.

--- src/grqe/fputil.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable, List
-
-
-def fold[X](xs: List[X], operator: Callable[[X, X], X]) -> X:
-    res = xs[0]
-    for x in xs[1:]:
-        res = operator(res, x)
-    return res
